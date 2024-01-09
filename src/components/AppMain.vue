71a026d4e5ff7215e4c97fcd4adbc56f7382ec89
zGiusee <giusebelforte@gmail.com>
2024-01-09 14:41:49 +0100

    [UPDATE] added comics section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,88 @@
 <script>
 import InfoBanner from '../components/InfoBanner.vue'
+import ComicApp from '../components/ComicApp.vue'
 
 export default {
     components: {
         InfoBanner,
+        ComicApp,
     },
     data() {
         return {
-
+            comics: [
+                {
+                    "thumb": "https://www.coverbrowser.com/image/action-comics/1-1.jpg",
+                    "price": "$19.99",
+                    "series": "Action Comics",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg",
+                    "price": "$3.99",
+                    "series": "American Vampire 1976",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg",
+                    "price": "$16.99",
+                    "series": "Aquaman",
+                    "type": "graphic novel"
+                },
+                {
+                    "thumb": "https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg",
+                    "price": "$2.99",
+                    "series": "Batgirl",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg",
+                    "price": "$3.99",
+                    "series": "Batman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg",
+                    "price": "$2.99",
+                    "series": "Batman Beyond",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/0/0d/Batman_Superman_Vol_1_1.jpg",
+                    "price": "$3.99",
+                    "series": "Batman/Superman",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/cf/Batman_Superman_Annual_Vol_2_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman/Superman Annual",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/5/54/Batman_The_Joker_War_Zone_Vol_1_1.jpg",
+                    "price": "$5.99",
+                    "series": "Batman: The Joker War Zone",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/6/64/Batman_Three_Jokers_Collected.jpg",
+                    "price": "$6.99",
+                    "series": "Batman: Three Jokers",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/f/f8/Batman_White_Knight_Presents_Harley_Quinn_Vol_1_1.jpg",
+                    "price": "$4.99",
+                    "series": "Batman: White Knight Presents: Harley Quinn",
+                    "type": "comic book"
+                },
+                {
+                    "thumb": "https://static.wikia.nocookie.net/marvel_dc/images/c/c8/Catwoman_Vol_2_1.jpg",
+                    "price": "$16.99",
+                    "series": "Catwoman",
+                    "type": "graphic novel"
+                }
+            ]
         }
     },
     methods: {
@@ -20,14 +95,14 @@ export default {
     <main>
 
         <!-- MAIN CONTENT SECTION -->
+
+        <!-- COMICS CARD SECTION-->
         <div class='comics-card-container'>
             <div class="container">
 
                 <div class="row">
-                    <!-- COMICS CARD SECTION-->
-                    <div class="col-12">
-                        <h3> Content goes here </h3>
-                    </div>
+                    <!-- COMICS CARD COMPONENT -->
+                    <ComicApp class="my-4" v-for="(comic, index) in comics" :key="index" :comic="comic"></ComicApp>
                 </div>
 
             </div>
@@ -48,6 +123,7 @@ export default {
 main {
     .comics-card-container {
         background-color: $dc_black;
+        padding: 70px 0px;
     }
 
     h3 {
---
+++ src/components/ComicApp.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+    props: {
+        comic: Object,
+    }
+}
+</script>
+
+<template>
+    <div class="col-2">
+        <div>
+            <div class="img-container p-relative">
+                <img :src="comic.thumb" :alt="comic.series">
+                <span class="price-tag">{{ comic.price }}</span>
+            </div>
+            <div>
+                <h5>{{ comic.series }}</h5>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+@use '../styles/partials/variables' as *;
+
+h5 {
+    color: white;
+    text-transform: uppercase;
+    margin-top: 20px;
+    font-size: 15px;
+}
+
+.price-tag {
+    color: white;
+    background-color: red;
+    padding: 2px 7px;
+    font-size: 13px;
+    border-radius: 10px;
+    position: relative;
+    bottom: 220px;
+    left: 153px;
+}
+
+.img-container {
+
+
+    img {
+        width: 190px;
+        height: 230px;
+        object-fit: cover;
+        object-position: top;
+
+    }
+
+}
+</style>
